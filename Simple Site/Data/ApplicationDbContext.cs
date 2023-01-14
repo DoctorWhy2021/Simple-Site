f8dbcab7d69a4d92dd5c4d63a4acd3b772b674ab
DoctorWhy2021 <vladyslav.sapeha@nure.ua>
2023-01-14 23:43:28 +0200

    Created JokesController with pattern for Entity Framework

--- Simple Site/Data/ApplicationDbContext.cs
+++ Simple Site/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Simple_Site.Models;
 
 namespace Simple_Site.Data;
 
@@ -9,4 +10,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<Simple_Site.Models.Jokes> Jokes { get; set; } = default!;
 }
